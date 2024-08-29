9f5bef3508f318c7ed0a13dbc5173054e9125bfa
chaeyoungeee <chaeinge@gmail.com>
2024-08-29 12:12:54 +0900

    [BE] init

---
+++ backend/src/main/java/limchaeyoung/daily_sprout/DailySproutApplication.java
@@ -0,0 +1,13 @@
+package limchaeyoung.daily_sprout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailySproutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailySproutApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/limchaeyoung/daily_sprout/DailySproutApplicationTests.java
@@ -0,0 +1,13 @@
+package limchaeyoung.daily_sprout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailySproutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

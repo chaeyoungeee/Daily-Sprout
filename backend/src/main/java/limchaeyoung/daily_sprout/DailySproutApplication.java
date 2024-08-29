fcc2d89a5e4bd62c4358f85cfdefeb37e728401b
chaeyoungeee <chaeinge@gmail.com>
2024-08-29 13:25:36 +0900

    [BE] feat: jpa auditing 기능 활성화

--- backend/src/main/java/limchaeyoung/daily_sprout/DailySproutApplication.java
+++ backend/src/main/java/limchaeyoung/daily_sprout/DailySproutApplication.java
@@ -2,8 +2,10 @@ package limchaeyoung.daily_sprout;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DailySproutApplication {
 
 	public static void main(String[] args) {
